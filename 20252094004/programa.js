f6198688c3d4a27eedf9cb4839d79bf4b7f1d5d2
Jorge <lizpe0102@gmail.com>
2025-08-15 20:45:20 -0500

    V 0.4 ......

--- 20252094004/programa.js
+++ 20252094004/programa.js
@@ -1,30 +1,38 @@
 // Coordenadas de Bogotá
-const bogota = turf.point([-74.08175, 4.60971]);
+const bogotaCoords = [-74.08175, 4.60971];
+const bogota = turf.point(bogotaCoords);
 
 // Crear el mapa
-const map = L.map('map').setView([4.60971, -74.08175], 2);
+const map = L.map('map').setView([20, 0], 2);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/">OSM</a> contributors'
+    attribution: '© <a href="https://www.openstreetmap.org/">OpenStreetMap</a> · © <a href="https://carto.com/">CARTO</a>'
 }).addTo(map);
 
-// Marcador en Bogotá
-L.marker([4.60971, -74.08175]).addTo(map).bindPopup("Bogotá, Colombia").openPopup();
+// Agregar marcador fijo en Bogotá
+L.marker([bogotaCoords[1], bogotaCoords[0]])
+    .addTo(map)
+    .bindPopup("<strong>Bogotá</strong><br>Capital de Colombia")
+    .openPopup();
 
-// Cargar GeoJSON de países desde URL
+// Cargar datos GeoJSON
 fetch('https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson')
     .then(res => res.json())
     .then(data => {
         L.geoJSON(data, {
-            onEachFeature: (feature, layer) => {
-                // Calcular distancia en km
+            onEachFeature: function (feature, layer) {
+                // Obtener nombre del país evitando undefined
+                const nombrePais = feature.properties.ADMIN || feature.properties.name || "País desconocido";
+
+                // Calcular distancia al centroide del país
                 const centroid = turf.centroid(feature);
                 const distancia = turf.distance(bogota, centroid, { units: 'kilometers' }).toFixed(2);
 
-                layer.bindPopup(`<strong>${feature.properties.ADMIN}</strong><br>Distancia: ${distancia} km`);
+                // Popup con formato solicitado
+                layer.bindPopup(`<strong>Distancia desde Bogotá a ${nombrePais}:</strong><br>${distancia} km`);
             },
             style: {
-                color: "#00FFFF",
+                color: '#00FFFF',
                 weight: 1
             }
         }).addTo(map);
