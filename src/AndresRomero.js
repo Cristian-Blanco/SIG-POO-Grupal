08e6b3de14e54a26553098546b3b07d3d4be413e
Jorge <lizpe0102@gmail.com>
2025-08-15 20:53:24 -0500

    version 0.3

--- src/AndresRomero.js
+++ src/AndresRomero.js
@@ -1,26 +1,44 @@
-// programa.js
+// Inicializar el mapa
+const map = L.map('map').setView([4.60524, -74.07674], 15);
 
-// Inicializar el mapa centrado en Colombia
-const map = L.map('map').setView([4.605240373027705, -74.07673993624353], 16);
-
-
-
-// Cargar la capa base de OpenStreetMap
+// Capa base OSM
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map); 
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
- src="https://code.jquery.com/jquery-3.6.0.min.js"
- 
-var estiloPoligonoBarrio = {
-    fillColor: "#09435C",       // Azul para el relleno
-    color: "#031821",           // Azul más oscuro para el borde
-    weight: 1,                  // Grosor del borde
-    opacity: 1                  // Opacidad del borde
-  };
- // Cargar el archivo GeoJSON del polígono del Barrio
-  $.getJSON('Lotes_La_Capuchina.geojson', function(data) {
-    L.geoJSON(data, {
-      style: estiloPoligonoBarrio
-    }).addTo(map);
-})
+// Estilo del polígono
+const estiloPoligono = {
+  fillColor: '#09435C',  // relleno
+  color: '#031821',      // borde
+  weight: 1,
+  opacity: 1,
+  fillOpacity: 0.5
+};
+
+// Cargar el GeoJSON
+fetch('Lotes_La_Capuchina.geojson')
+  .then(resp => {
+    if (!resp.ok) throw new Error('No se pudo cargar el GeoJSON');
+    return resp.json();
+  })
+  .then(data => {
+    const layer = L.geoJSON(data, { style: estiloPoligono }).addTo(map);
+    const bounds = layer.getBounds();
+    if (bounds.isValid()) map.fitBounds(bounds, { padding: [20, 20] });
+  })
+  .catch(err => {
+    console.error('Error cargando GeoJSON:', err);
+    alert('Hubo un problema cargando el polígono. Revisa la consola.');
+  });
+
+// === Marcadores ===
+const marcador1 = L.marker([4.601882386879739, -74.07840290587482])
+  .bindPopup("📍 Marcador 1")
+  .addTo(map);
+
+const marcador2 = L.marker([4.604600183830083, -74.07868720364645])
+  .bindPopup("📍 Marcador 2")
+  .addTo(map);
+
+
