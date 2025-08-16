861a4b5d0d2dc9e5db141216934ac8ad8ca1c569
Jorge <lizpe0102@gmail.com>
2025-08-15 21:09:13 -0500

    v6.0

--- docs/jonatanariza.js
+++ docs/jonatanariza.js
@@ -9,11 +9,11 @@ require([
         basemap: "streets-navigation-vector"
     });
 
-    // Crear la vista del mapa
+    // Crear la vista del mapa, centrada en Bogotá
     const view = new MapView({
         container: "viewDiv",
         map: map,
-        center: [-74.0721, 4.6097], // Coordenadas de Bogotá
+        center: [-74.0721, 4.6097],
         zoom: 11
     });
 
@@ -21,50 +21,33 @@ require([
     view.ui.remove("attribution");
     view.ui.empty("top-left");
 
-    // 1. Capa de feminicidios (la capa de puntos que ya tienes)
-    const feminicidiosLayer = new FeatureLayer({
+    // Definir la plantilla de información emergente (Pop-up Template)
+    const popupTemplate = {
+        title: "Registro de Incidente",
+        content: "<b>Localidad:</b> {LOCALIDAD}<br><b>Año:</b> {ANIO_HECHO}<br><b>Descripción:</b> {TIPO_VIOLENCIA_GENERO}"
+    };
+
+    // Crear la capa de características desde el servicio REST de Bogotá
+    const featureLayer = new FeatureLayer({
         url: "https://serviciosgis.catastrobogota.gov.co/arcgis/rest/services/mujeres/derechovidalibredeviolencias/MapServer/1",
-        outFields: ["LOCALIDAD", "ANIO_HECHO", "TIPO_VIOLENCIA_GENERO"],
-        visible: false // Hacemos esta capa invisible para que no se superponga
-    });
-
-    // 2. Capa de polígonos de localidades
-    const localidadesLayer = new FeatureLayer({
-        url: "https://serviciosgis.catastrobogota.gov.co/arcgis/rest/services/Limites_Catastrales/MapServer/1", // URL del servicio de localidades
-        outFields: ["Nombre_Localidad"], // Solo se necesitan los campos de nombre
-        popupTemplate: {
-            title: "Localidad de Bogotá",
-            content: async function(event) {
-                // Obtener el nombre de la localidad
-                const nombreLocalidad = event.graphic.attributes.Nombre_Localidad;
-
-                // Consulta a la capa de feminicidios para contar los casos en la geometría de la localidad
-                const query = feminicidiosLayer.createQuery();
-                query.geometry = event.graphic.geometry; // Usar la geometría del polígono de la localidad como filtro
-                query.outStatistics = [{
-                    statisticType: "count",
-                    onStatisticField: "OBJECTID",
-                    outStatisticFieldName: "total_feminicidios"
-                }];
-
-                try {
-                    const results = await feminicidiosLayer.queryFeatures(query);
-                    const totalAsesinatos = results.features[0].attributes.total_feminicidios || 0;
-                    
-                    return `
-                        <b>Localidad:</b> ${nombreLocalidad}
-                        <br>
-                        <b>Total de asesinatos:</b> ${totalAsesinatos}
-                    `;
-                } catch (error) {
-                    console.error("Error al consultar los feminicidios:", error);
-                    return "No se pudieron obtener los datos de asesinatos.";
+        outFields: ["LOCALIDAD", "ANIO_HECHO", "TIPO_VIOLENCIA_GENERO", "OBJECTID"],
+        popupTemplate: popupTemplate,
+        // Configurar un renderizador para mostrar los puntos como marcadores
+        renderer: {
+            type: "simple",
+            symbol: {
+                type: "simple-marker",
+                style: "circle",
+                color: [255, 0, 0, 0.8], // Color rojo semi-transparente
+                size: "8px",
+                outline: {
+                    color: [255, 255, 255, 0.8],
+                    width: 1
                 }
             }
         }
     });
 
-    // Añadir ambas capas al mapa
-    map.add(localidadesLayer);
-    map.add(feminicidiosLayer); // Se agrega para poder consultarla
+    // Añadir la capa al mapa
+    map.add(featureLayer);
 });
